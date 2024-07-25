2ea42d4c9431087b71dbc22dcc64aad97ec4d2ab
amnayousufiqbal <amnayousuf2002@gmail.com>
2024-07-25 20:49:51 +0500

    question # 3

---
+++ amna2.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<String> studentDetails = [
+    'Name: John Doe',
+    'Class: 10',
+    'Roll No: 12345',
+    'Grade: A',
+    'Percentage: 90%'
+  ];
+  print(studentDetails);
+}
