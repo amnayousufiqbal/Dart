295bdbd7cec4c29103d1bd1d59c9b0c08e2d91f4
amnayousufiqbal <amnayousuf2002@gmail.com>
2024-08-02 16:20:15 +0500

    assigenment # 5

---
+++ amna.dart
@@ -0,0 +1,14 @@
+void main() {
+  List<String> names = [
+    "amna",
+    "ahmad",
+    "ibad",
+    "shoaib",
+    "ammar",
+    "ayesha",
+    "zainab",
+    "waniya",
+    "arham"
+  ];
+  print(names);
+}
